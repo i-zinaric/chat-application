affb0825711dcf8297e18ae327324188bd2765cb
zinaric <zinaric@gmail.com>
2023-03-08 08:45:13 +0100

    final

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,7 @@
 import logo2 from '../images/logo-2.svg';
 import header from '../styles/header.scss';
-import astronaut from '../images/avatar-1.svg';
-import ninja from '../images/avatar-2.svg';
-import mask from '../images/avatar-3.svg';
 
 const Header = (props) => {
-
-    const avatars = [
-        {image: astronaut, nameId: 'astronaut'},
-        {image: ninja, nameId: 'ninja'},
-        {image: mask, nameId: 'mask'}
-    ];
-
-    const headerAvatars = props.headerAvatars;
 
     return (
         <div className='header'>
